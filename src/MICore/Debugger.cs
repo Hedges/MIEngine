048167f0f056851fc3c3d47793109d88a3184dca
Jarek Syrylak <hedges@resync.pl>
2023-07-30 18:24:04 +0100

    Fix for my commit from 14/03/2022 (SHA-1: 95d5fca6ae5464bdb961508bab697e9513b8d4f5) that might have broken some debugging functionality - I saw some broken strings coming while debugging the debugger :)

--- src/MICore/Debugger.cs
+++ src/MICore/Debugger.cs
@@ -1288,8 +1288,17 @@ namespace MICore
                 switch (c)
                 {
                     case '~':
-                    case '@':
                         OnDebuggeeOutput(noprefix);         // Console stream
+                        break;
+                    case '@':
+                        if ((this.MICommandFactory.Mode == MIMode.Gdb) || (_launchOptions is UnixShellPortLaunchOptions))
+                        {
+                            OnDebuggeeOutput(originalLine + '\n');
+                        }
+                        else
+                        {
+                            OnDebuggeeOutput(noprefix);         // Console stream
+                        }
                         break;
                     case '^':
                         OnResult(noprefix, token);
