9bcd0ca5a408df172dd2e4b75cef4f64f77b2b71
Jarek Syrylak <hedges@resync.pl>
2024-09-03 17:41:36 +0100

    Detect ARM64 architecture under LLDB, which is quite important on macOS nowadays... :)

--- src/MICore/CommandFactories/lldb.cs
+++ src/MICore/CommandFactories/lldb.cs
@@ -144,6 +144,10 @@ namespace MICore
                         {
                             return TargetArchitecture.X86;
                         }
+                        else if (resultLine.IndexOf("arm64", StringComparison.OrdinalIgnoreCase) >= 0)
+                        {
+                            return TargetArchitecture.ARM64;
+                        }
                         else if (resultLine.IndexOf("aarch64", StringComparison.OrdinalIgnoreCase) >= 0)
                         {
                             return TargetArchitecture.ARM64;
